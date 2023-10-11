7631f0ab4d1f9af7c78acf09e6374ee4b989cf89
jyax <yaxjacob@msu.edu>
2023-10-11 17:24:49 -0400

    Basic API calls established with python-steam-api wrapper. Restructured to contain src folder.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from steam import Steam
+from datetime import datetime
+import configparser
+import json
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+key = config.get('api', 'steamapikey')
+
+sc = Steam(key)
+result = sc.users.get_owned_games("76561198203717873")
+print(result)
+with open('temp.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f, ensure_ascii=False, indent=4)
+
